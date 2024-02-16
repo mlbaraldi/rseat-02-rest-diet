89952bba971fe720388bd62d4a850a9e1417f523
Marcelo Baraldi <mlbaraldi@hotmail.com>
2024-02-15 23:53:28 -0300

    feat: validate logged user

--- middlewares/check-user-id.ts
+++ middlewares/check-user-id.ts
@@ -1,10 +1,20 @@
-import { FastifyReply, FastifyRequest } from 'fastify'
+import { FastifyRequest, FastifyReply } from 'fastify'
+import { knex } from '../db/database'
 
 export async function checkUserId(req: FastifyRequest, res: FastifyReply) {
   const { userId } = req.cookies
+
   if (!userId) {
     return res.status(401).send({
-      error: 'User not logged',
+      error: 'User not logged. Please create a User POST /user',
+    })
+  }
+
+  const user = await knex('users').where('id', userId)
+
+  if (user.length === 0) {
+    return res.status(401).send({
+      error: 'User does not exist. Please verify userId',
     })
   }
 }
