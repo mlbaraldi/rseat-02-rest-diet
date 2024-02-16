833eedb58e2e40e2aefe7403b06a802b756cd168
Marcelo Baraldi <mlbaraldi@hotmail.com>
2024-02-16 01:51:36 -0300

    chore: add get summary user route

--- routes/user.ts
+++ routes/user.ts
@@ -2,6 +2,7 @@ import { randomUUID } from 'node:crypto'
 import { FastifyInstance } from 'fastify'
 import { knex } from '../db/database'
 import { createUserSchema } from '../schema'
+import { checkUserId } from '../middlewares/check-user-id'
 
 export async function userRoutes(app: FastifyInstance) {
   app.post('/user', async (req, res) => {
@@ -26,12 +27,36 @@ export async function userRoutes(app: FastifyInstance) {
     res.status(201).send(`User ID: ${userId} created successfully`)
   })
 
-  app.get('/user', async (req, res) => {
+  app.get('/user', { preHandler: checkUserId }, async (req, res) => {
     const id = req.cookies.userId
-    if (!id) {
-      res.status(401).send('User not authorized. Please create with /POST')
+
+    const meals = await knex('meals').where('userId', id).orderBy('datetime')
+
+    const totalMeals = meals.length
+    const totalMealsOnDiet = meals.filter((meal) => meal.isDiet === 1).length
+    const totalMealsOffDiet = totalMeals - totalMealsOnDiet
+
+    let currentStreak = 0
+    let maxStreak = 0
+
+    for (let i = 0; i < totalMeals; i++) {
+      if (meals[i].isDiet === 1) {
+        currentStreak++
+        if (currentStreak > maxStreak) {
+          maxStreak = currentStreak
+        }
+      } else {
+        currentStreak = 0
+      }
     }
-    const user = await knex('users').where({ id }).first()
-    return { user }
+
+    const response = {
+      User: id,
+      'Total meals': totalMeals,
+      'Total meals on diet': totalMealsOnDiet,
+      'Total meals off diet': totalMealsOffDiet,
+      'Diet Streak!': currentStreak,
+    }
+    res.status(200).send(response)
   })
 }
