d47286ad932698ae59fed40b480640209e702619
Marcelo Baraldi <mlbaraldi@hotmail.com>
2024-02-15 22:48:42 -0300

    chore: add cookie control and user routes

--- routes/user.ts
+++ routes/user.ts
@@ -1,18 +1,41 @@
-import { randomUUID } from 'crypto'
+import { randomUUID } from 'node:crypto'
 import { FastifyInstance } from 'fastify'
+import { knex } from '../db/database'
 import z from 'zod'
+
 export async function userRoutes(app: FastifyInstance) {
-  app.get('/user', (req) => {
+  app.post('/user', async (req, res) => {
     const createUserSchema = z.object({
-      id: randomUUID(),
       name: z.string(),
     })
-    const data = req.body
+    const { name } = createUserSchema.parse(req.body)
 
-    console.log('entrou em user')
+    let userId = req.cookies.userId
+
+    if (userId) {
+      res.status(401).send(`User ID: ${userId} already created and logged in`)
+    }
+
+    userId = randomUUID()
+    res.cookie('userId', userId, {
+      path: '/',
+      maxAge: 60 * 60 * 24, // 1 day
+    })
+    await knex('users').insert({
+      id: userId,
+      name,
+      created_at: new Date(),
+    })
+
+    res.status(201).send(`User ID: ${userId} created successfully`)
   })
 
-  app.post('/user', () => {
-    console.log('postou em user')
+  app.get('/user', async (req, res) => {
+    const id = req.cookies.userId
+    if (!id) {
+      res.status(401).send('User not authorized. Please create with /POST')
+    }
+    const user = await knex('users').where({ id }).first()
+    return { user }
   })
 }
--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,16 @@
 import fastify from 'fastify'
 import { userRoutes } from '../routes/user'
 import { mealsRoutes } from '../routes/meals'
+import fastifyCookie from '@fastify/cookie'
 
 const app = fastify()
 
-app.register(mealsRoutes)
+app.addHook('preHandler', async (req) => {
+  console.log(`[${req.method}] ${req.url}`)
+})
+
+app.register(fastifyCookie)
 app.register(userRoutes)
+app.register(mealsRoutes)
 
 export default app
